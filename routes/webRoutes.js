296f45f3fa221de73b12a74835d7f70ccb176aa6
DaniloSsoares <danilo.ssoares201@gmail.com>
2025-05-18 23:57:43 -0300

    Finalizando todas as funcionalidades do Site com firebase

--- routes/webRoutes.js
+++ routes/webRoutes.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const router = express.Router();
 const db = require('../config/firebaseConfig');
@@ -6,17 +7,34 @@ const db = require('../config/firebaseConfig');
     res.render('home');
      });
 
-     router.get('/consulta', (req, res) => {
-        res.render('Consulta/consulta');
-     });
-
+     router.get('/consulta', async(req, res) => {
+      
+      db.collection("agendamentos").get().then((snapshot)=>{
+         const posts = snapshot.docs.map(doc =>{
+            return{
+               id: doc.id,
+               ...doc.data()
+            }
+         })
+        res.render('Consulta/consulta', {posts:posts});
+          console.log(posts)
+      }) 
+         })
+          
+            
      router.get("/editar/:id", function(req, res){
-        res.render('Editar/editar');
-     })
+          db.collection("agendamentos").doc(req.params.id).get().then((doc)=>{
+            res.render('/consulta', { post: {id: doc.id, ...doc.data() }});
+          })
+            })
+   
 
      router.get('/excluir/:id', function(req, res){
-        
+      db.collection("agendamentos").doc(req.params.id).delete().then(function(){
+            console.log('Documento atualizado com sucesso');
+              res.redirect('/consulta');
      })
+       })
 
      router.get('/cadastro', (req, res) => {
         res.render('Cadastro/cadastro');
@@ -38,18 +56,20 @@ const db = require('../config/firebaseConfig');
 
 
      router.post('/editar/:id', function(req, res){
-         post.update({
+         db.collection("agendamentos").doc(req.params.id).update({
              nome:req.body.nome,
              email:req.body.email,
              telefone:req.body.telefone,
              data_contato:req.body.data_contato,
              observacao:req.body.observacao,
              origem:req.body.origem
-          },{where:{id: req.params.id}}).then(function(){
-             res.redirect('/consulta')
-         }).catch(function(erro){
-             res.send('Erro ao atualizar o post'+erro)
-         })
-     })
+          }).then(function(){
+            console.log('Documento atualizado com sucesso');
+              res.redirect('/consulta');
+            }).catch(function(error){
+             console.error('Erro ao atualizar o documento:', error);
+           res.status(500).send('Erro ao atualizar o documento: ' + error.message);
+         });
+       });
 
 module.exports = router;
